694e38c21cb6c81ba14af5f8166cbf7aabc948bc
AsumaToki04 <megamanzxu@hotmail.com>
2024-11-19 20:25:53 -0500

    ContentView03 - Fase 1

---
+++ Sesion03/ContentView03.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView03.swift
+//  Sesion03
+//
+//  Created by DAMII on 19/11/24.
+//
+
+import SwiftUI
+
+class ModeloDatos: ObservableObject {
+    @Published var personas: [String] = ["Juan", "Nadia", "Maria"]
+}
+
+struct ContentView03: View {
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
